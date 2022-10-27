c11b0d0c87ff1ccaa3455eb038f7a6c2374a8212
Phoques <dylan.munro6@studytafensw.edu.au>
2022-10-27 13:25:27 +1100

    Added notes regarding Binary

--- Assets/_NavMesh/Scripts/AreaMovementModifier.cs
+++ Assets/_NavMesh/Scripts/AreaMovementModifier.cs
@@ -20,13 +20,24 @@ public class AreaMovementModifier : MonoBehaviour
 
     private void Update()
     {
-        NavMeshHit hit;
-        _agent.SamplePathPosition(-1, 0.0f, out hit);
+        //Unity Engine.AI is required for Navmesh
+        // SamplePathPosition is a function that returns 
+        NavMeshHit hit; 
+        _agent.SamplePathPosition(-1, 0.0f, out hit); //grab the type of floor we are on.
 
-        int GrassMask = 1 << NavMesh.GetAreaFromName("Tall Grass");
-        int filtered = hit.mask & GrassMask;
+        // Grassmask = 1 bitshift left until 'TallGrass' is found? Binary wise it shifts the 1 to the left 000000001< 000000010
+        // User 4 is 16,        64 32 16 8  4  2  1 
+        //                      0  0  1  0  0  0  0
+        int GrassMask = 1 << NavMesh.GetAreaFromName("Tall Grass"); // User 4 is 16, 64 32 16 8  4  2   1 0  0  1  0  0  0   0
+        int filtered = hit.mask & GrassMask; // Hit Mask is the Navmesh check, GrassMask is our check, both need to be true to trigger the mask
+        //hitmask =   0000010000
+        //GrassMask=  0000010000 This would be true, meaning we are touching grass.
+
         //Using binary (see picture on phone) 0 means we didnt hit the grass
         // 16 means we hit the grass.
+
+        // the '10' that is next to Tall Grass in the inspector, is trhe 'weight value' e.g it takes 10 time longer/ harder to go through grass
+        //So in theory the Navmesh will avoid this terrain unless its 10x shorter to go through, or has no other pathing choices.
 
         if (filtered == 0)
         {
